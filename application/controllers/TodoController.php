a246e0e99e27f34d9076668d3ab3794f8de1631f
mckie <rejayflora@gmail.com>
2023-02-10 13:14:19 +0800

    Txt file Deleted

--- application/controllers/TodoController.php
+++ application/controllers/TodoController.php
@@ -6,13 +6,21 @@ class TodoController extends MY_Controller {
 	public function __construct() {
 		parent::__construct();
 		// load model here
+		$this->load->database();
+		
 	}
 
 	public function index()
 	{
+		
 		$this->modules[]      = 'todo';
         $this->js_listeners[] = '';
         $this->layout('welcome_message');
+	}
+	public function get_data(){
+
+		$query = $this->db->get('mckie');
+		return $query->result_array();
 	}
 	
 }
