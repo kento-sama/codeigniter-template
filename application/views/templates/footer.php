3401cf32a6ecf14be639697135501951923518aa
Amoby <amboyjd1996@gmail.com>
2023-02-14 19:00:09 +0800

    adding depedencies

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,12 +6,38 @@
 <script>
     var base_url = "<?php echo base_url();?>";
 </script>
-
-<!--   Core JS Files   -->
-<!-- <script src="<?php echo base_url();?>assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url();?>assets/js/bootstrap.min.js" type="text/javascript"></script> -->
-
-
+!-- jQuery -->
+<script src="<?php echo base_url('assets/plugins/jquery/jquery.min.js');?>"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?php echo base_url('assets/plugins/jquery-ui/jquery-ui.min.js"');?>"></script>
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
+<!-- ChartJS -->
+<script src="<?php echo base_url('assets/plugins/chart.js/Chart.min.js');?>"></script>
+<!-- Sparkline -->
+<script src="<?php echo base_url('aassets/plugins/sparklines/sparkline.js');?>"></script>
+<!-- JQVMap -->
+<script src="<?php echo base_url('assets/plugins/jqvmap/jquery.vmap.min.js');?>"></script>
+<script src="<?php echo base_url('assets/plugins/jqvmap/maps/jquery.vmap.usa.js');?>"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo base_url('assets/plugins/jquery-knob/jquery.knob.min.js');?>"></script>
+<!-- daterangepicker -->
+<script src="<?php echo base_url('assets/plugins/moment/moment.min.js');?>"></script>
+<script src="<?php echo base_url('assets/plugins/daterangepicker/daterangepicker.js');?>"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?php echo base_url('assests/plugins/daterangepicker/daterangepicker.js');?>"></script>
+<script src="<?php echo base_url('assests/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js');?>"></script>
+<!-- Summernote -->
+<script src="<?php echo base_url('assets/plugins/summernote/summernote-bs4.min.js');?>"></script>
+<!-- overlayScrollbars -->
+<script src="<?php echo base_url('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js');?>"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url('assets/plugins/overlayScrollbars/js/adminlte.js');?>"></script>
 <script src="<?php echo base_url('assets/js/helper.js');?>"></script>
 
 <!--   JS plugins -->
