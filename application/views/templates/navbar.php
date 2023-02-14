889ebc114f0e50d7318b31176219a51a264540b6
Melcris <melcris.opon@gmail.com>
2023-02-14 20:33:38 +0800

    Added necessary code

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark" style="margin-bottom:20px;">
   <a class="navbar-brand" href="#">
-    <img src="assets/img/xiaomi.png" width="60" height="25" class="d-inline-block align-top" style="margin-left:20px;">
+    <img src="<?php echo base_url();?>assets/img/xiaomi.png" width="60" height="25" class="d-inline-block align-top" style="margin-left:20px;">
   </a>
 </nav>
