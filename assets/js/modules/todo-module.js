b76a71766a005f500042f9fbcaa4a92615f8499d
kento-sama <kentharvey.pecorro92@gmail.com>
2023-02-16 12:14:58 +0800

    kent-feature-2 resolve conflict

--- assets/js/modules/todo-module.js
+++ assets/js/modules/todo-module.js
@@ -3,14 +3,40 @@ Todo = function () {
 	var property = {};
 
 	var fn = function() {
+		function add_todo() {
+			$('button#add-todo').unbind();
+			$('button#add-todo').on('click', function() {
+				modal.add_modal();
+
+				return false;
+			});
+		}
+
+		return {
+			add_todo: add_todo
+		}
 	}();
 
 	var table = function() {
+		function generate_table() {
+			// TODO: generate datatable here
+			$('table#sample-table').DataTable().destroy();
+			$('table#sample-table').DataTable();
+		}
 
+		return {
+			generate_table: generate_table
+		}
 	}();
 
 	var modal = function() {
+		function add_modal() {
+			console.log("open modal");
+		}
 
+		return {
+			add_modal: add_modal
+		}
 	}();
 
 	return {
@@ -19,4 +45,4 @@ Todo = function () {
 		table   : table,
 		modal	: modal
 	}
-}
+}();
