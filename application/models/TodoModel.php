86c956abae4205e39f1b6659ccdaf99a20f67136
Amoby <amboyjd1996@gmail.com>
2023-02-16 19:49:12 +0800

    feature using modals and modules with correction

--- application/models/TodoModel.php
+++ application/models/TodoModel.php
@@ -2,8 +2,6 @@
     class TodoModel extends CI_Model{
         public function insertList($data){
             $this->load->database();
-            //$this->db->insert("table_name",$data);
-            
             return $this->db->insert("todo",$data);
         }
     }
