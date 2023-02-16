22659ca00a5daca3052d6865e195cc1814c6f236
Amoby <amboyjd1996@gmail.com>
2023-02-16 20:04:36 +0800

    feature using modals and modules with correction

--- application/views/todolist.php
+++ application/views/todolist.php
@@ -18,25 +18,3 @@
     </div>
 </div>
 
-<!-- Modal -->
-<div class="modal fade" id="create_list" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">ADD ITEM TO TODOLIST</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-            <label>TO DO ITEM</label>
-            <input class="form-control" id="item" name="item" required/>
-      </div>
-      <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <!-- <button type="button" class="btn btn-primary" onclick="test(item.value)">Create</button> -->
-            <button type="button" class="btn btn-primary">Create</button>
-      </div>
-    </div>
-  </div>
-</div>
